bf09db8bc0b566a7cf4630c9773915968c56297c
DawidRaczek <dawid.raczek6666@gmail.com>
2024-01-27 00:01:51 +0100

    added cv to firebase

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
-import '../styles/header.css'
-import {Button} from "@mui/material";
-import {useNavigate} from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import '../styles/header.css';
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
+import firebaseConfig from '../firebaseconfig';
 
 const Header = () => {
-
     const navigate = useNavigate();
+    const [cvDownloadUrl, setCvDownloadUrl] = useState(null);
+
+    useEffect(() => {
+        const storage = getStorage();
+
+        const cvRef = ref(storage, 'Dawid Rak CV PL.pdf'); 
+        getDownloadURL(cvRef)
+            .then((url) => {
+                setCvDownloadUrl(url);
+            })
+            .catch((error) => {
+                console.error("Error getting CV download URL: ", error);
+            });
+    }, []);
+
+    const handleDownloadCV = () => {
+        if (cvDownloadUrl) {
+            const link = document.createElement('a');
+            link.href = cvDownloadUrl;
+            link.download = 'Dawid_Rak_CV_PL.pdf'; 
+            document.body.appendChild(link);
+
+            link.click();
+
+            document.body.removeChild(link);
+        }
+    };
 
     return (
         <header>
@@ -15,9 +43,7 @@ const Header = () => {
             </div>
             <div className="header-right">
                 <Button onClick={() => navigate('/')}>Home</Button>
-                <a href='../assets/Dawid Rak CV PL.pdf' download>
-                <Button>Get my CV</Button>
-                </a>
+                <Button onClick={handleDownloadCV}>Get my CV</Button>
                 <Button onClick={() => navigate('/projects')}>Projects</Button>
                 <Button onClick={() => navigate('/contact')}>Contact</Button>
             </div>
