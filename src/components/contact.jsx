7572a83669b7e88a06f80f95e958db0d05dc49c5
DawidRaczek <dawid.raczek6666@gmail.com>
2024-02-01 19:29:31 +0100

    some refactor and motion on contact page

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -3,50 +3,61 @@ import '../styles/contact.css';
 import { Button, Tooltip } from "@mui/material";
 import LinkedInIcon from "@mui/icons-material/LinkedIn.js";
 import { Email, GitHub } from "@mui/icons-material";
+import { motion } from "framer-motion";
 
 const Contact = () => {
-    const linkStyle = { textDecoration: 'none', color: 'white' };
-    const [isCopiedTooltipOpen, setIsCopiedTooltipOpen] = useState(false);
+  const linkStyle = { textDecoration: 'none', color: 'white' };
+  const [isCopiedTooltipOpen, setIsCopiedTooltipOpen] = useState(false);
 
-    const copyEmailToClipboard = () => {
-        const emailElement = document.createElement("textarea");
-        emailElement.value = "dawidrak.contact@gmail.com";
-        document.body.appendChild(emailElement);
+  const copyEmailToClipboard = () => {
+    const emailElement = document.createElement("textarea");
+    emailElement.value = "dawidrak.contact@gmail.com";
+    document.body.appendChild(emailElement);
 
-        emailElement.select();
-        document.execCommand("copy");
+    emailElement.select();
+    document.execCommand("copy");
 
-        document.body.removeChild(emailElement);
+    document.body.removeChild(emailElement);
 
-        setIsCopiedTooltipOpen(true);
-        setTimeout(() => {
-            setIsCopiedTooltipOpen(false);
-        }, 4000);
-    };
+    setIsCopiedTooltipOpen(true);
+    setTimeout(() => {
+      setIsCopiedTooltipOpen(false);
+    }, 4000);
+  };
 
-    return (
-        <>
-            <div className='contact-page'>
-                <div className='container'>
-                    <h1 className='contact-title'>Contact me</h1>
-                    <Tooltip title="Click to copy email" placement="top">
-                    <Tooltip open={isCopiedTooltipOpen} title="Copied to clipboard" placement="top">
-                        <span style={{ display: 'flex' }}></span>
-                    </Tooltip>
-                        <Button className='contactbtn' variant='contained' color='success' onClick={copyEmailToClipboard}>
-                            E-mail<Email htmlColor='white' />
-                        </Button>
-                    </Tooltip>
-                        <Button className='contactbtn' variant='contained' component="a" href="https://www.linkedin.com/in/dawid-rak-158110293/" target="_blank" style={linkStyle}>
-                            LinkedIn<LinkedInIcon htmlColor='white' />
-                        </Button>
-                        <Button className='contactbtn' variant='contained' color='secondary' component="a" href="https://github.com/DawidRaczek" target="_blank" style={linkStyle}>
-                            GitHub<GitHub htmlColor='white' />
-                        </Button>
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className='contact-page'>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 0.8,
+            delay: 0.3,
+            ease: [0, 0.71, 0.2, 1.01],
+          }}
+        >
+          <div className='container'>
+            <h1 className='contact-title'>Contact me</h1>
+            <Tooltip title="Click to copy email" placement="top">
+              <Tooltip open={isCopiedTooltipOpen} title="Copied to clipboard" placement="top">
+                <span style={{ display: 'flex' }}></span>
+              </Tooltip>
+              <Button className='contactbtn' variant='contained' color='success' onClick={copyEmailToClipboard}>
+                E-mail<Email htmlColor='white' />
+              </Button>
+            </Tooltip>
+            <Button className='contactbtn' variant='contained' component="a" href="https://www.linkedin.com/in/dawid-rak-158110293/" target="_blank" style={linkStyle}>
+              LinkedIn<LinkedInIcon htmlColor='white' />
+            </Button>
+            <Button className='contactbtn' variant='contained' color='secondary' component="a" href="https://github.com/DawidRaczek" target="_blank" style={linkStyle}>
+              GitHub<GitHub htmlColor='white' />
+            </Button>
+          </div>
+        </motion.div>
+      </div>
+    </>
+  );
 }
 
 export default Contact;
--- src/components/front.jsx
+++ src/components/front.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import {Button} from "@mui/material";
-import '../styles/front.css'
-import {useNavigate} from "react-router-dom";
+import { Button } from "@mui/material";
+import '../styles/front.css';
+import { useNavigate } from "react-router-dom";
 import { motion } from 'framer-motion';
 
-const front = () => {
-
+const Front = () => {
     const navigate = useNavigate();
 
     return (
@@ -15,37 +14,37 @@ const front = () => {
                     initial={{ opacity: 0, scale: 0.5 }}
                     animate={{ opacity: 1, scale: 1 }}
                     transition={{
-                    duration: 0.8,
-                    delay: 0.5,
-                    ease: [0, 0.71, 0.2, 1.01]
+                        duration: 0.8,
+                        delay: 0.5,
+                        ease: [0, 0.71, 0.2, 1.01]
                     }}
-                    >
-                    <h1>HEY, I`M DAWID RAK</h1>
+                >
+                    <h1>HEY, I'M DAWID RAK</h1>
                 </motion.div>
                 <motion.div
                     initial={{ opacity: 0, scale: 0.5 }}
                     animate={{ opacity: 1, scale: 1 }}
                     transition={{
-                    duration: 0.8,
-                    delay: 1,
-                    ease: [0, 0.71, 0.2, 1.01]
+                        duration: 0.8,
+                        delay: 1,
+                        ease: [0, 0.71, 0.2, 1.01]
                     }}
-                    >
-                <p>A frontend developer with expertise in JavaScript and proficiency in the React library.</p>
-                <p>My passion lies in creating immersive web experiences, and I bring a strong commitment to clean,
-                    efficient code and a keen eye for user-centric design to every project.</p>
-                <p>With a solid foundation in frontend technologies, I thrive on transforming creative ideas into
-                    seamless and responsive applications.</p>
+                >
+                    <p>A frontend developer with expertise in JavaScript and proficiency in the React library.</p>
+                    <p>My passion lies in creating immersive web experiences, and I bring a strong commitment to clean,
+                        efficient code and a keen eye for user-centric design to every project.</p>
+                    <p>With a solid foundation in frontend technologies, I thrive on transforming creative ideas into
+                        seamless and responsive applications.</p>
                 </motion.div>
                 <motion.div
                     initial={{ opacity: 0, scale: 0.5 }}
                     animate={{ opacity: 1, scale: 1 }}
                     transition={{
-                    duration: 0.8,
-                    delay: 1,
-                    ease: [0, 0.71, 0.2, 1.01]
+                        duration: 0.8,
+                        delay: 1,
+                        ease: [0, 0.71, 0.2, 1.01]
                     }}
-                    >
+                >
                     <Button color='success' variant='contained' onClick={() => navigate('/projects')}>Projects</Button>
                 </motion.div>
             </div>
@@ -53,4 +52,4 @@ const front = () => {
     );
 }
 
-export default front;
+export default Front;
